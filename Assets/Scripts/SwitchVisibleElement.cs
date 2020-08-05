c593fcf413883d2d5cca571710f89d45944760c4
skuterus67 <milosstan@autograf.pl>
2020-08-05 22:11:23 +0200

    Day 1 - first scene with clock and screen transitions

---
+++ Assets/Scripts/SwitchVisibleElement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class SwitchVisibleElement : MonoBehaviour, IPointerClickHandler
+{
+    [SerializeField] GameObject oldElement;
+    [SerializeField] GameObject newElement;
+    [SerializeField] bool replace;
+    [SerializeField] string application;
+    [SerializeField] string key;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if (replace)
+        {
+            oldElement.SetActive(false);
+            newElement.SetActive(true);
+        }
+        else
+        {
+            newElement.SetActive(true);
+        }
+    }
+}
